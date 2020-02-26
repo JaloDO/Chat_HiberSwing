519f40309f2267ccb73065ef32108503662f8bcf
ivanjo91 <50482763+ivanjo91@users.noreply.github.com>
2020-02-26 21:25:17 +0100

    correccion

--- src/Controlador/Principal.java
+++ src/Controlador/Principal.java
@@ -1,9 +1,21 @@
 package Controlador;
 
+import Modelo.Conector;
+
 public class Principal {
 
 	
 	public static void main(String[] args) {
+		
+		//Ventana de la aplicación
+		
+				
+		//Componente de acceso y manipulación a la BD Biblioteca
+		Conector chat = new Conector();
+				
+		if(chat.getEm()!=null) {
+			
+		}
 		
 	}
 }
